df44d87df902cc7d29e61df9402c49f9d79afef6
eladyarkoni <eladyarkoni@gmail.com>
2010-11-06 15:14:01 +0200

    d

--- src/javastudyproject2/Main.java
+++ src/javastudyproject2/Main.java
@@ -10,17 +10,14 @@ package javastudyproject2;
  * @author EladYarkoni
  */
 public class Main {
-    // elad yarkoni's main
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+
+        
     }
 
-    public void foo1()
-    {
-
-    }
 
 }
